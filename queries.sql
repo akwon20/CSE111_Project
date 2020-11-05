d235f4a94cbc6145cbfacab781ab9a390a4b8e44
Austin Kwon <austinkwon@Austins-MacBook-Pro.local>
2020-11-05 15:42:22 -0800

    Added insert queries

---
+++ queries.sql
@@ -0,0 +1,19 @@
+
+-- Search query
+SELECT ()
+
+-- Insert query
+INSERT INTO Product(s_Name, p_name, p_price, p_release date, p_lastShipment[DATE] ), Store(s_cityID, s_storeName,  s_storeNum, p_Name)
+VALUES();
+
+INSERT INTO locations(storeName, storeNum, c_ID, c_name)
+VALUES();
+
+INSERT INTO SupplyDemand(p_name, p_price, p_lastShipment[DATE], r_nintendoRating, r_storeRating)
+VALUES();
+
+INSERT INTO priceChngFreq(p_name, p_price, p_lastShipment[DATE], b_name, b_basePrice)
+VALUES();
+
+
+-- Delete query
