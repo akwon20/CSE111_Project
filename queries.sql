025f54b48c7b1f0dd55c12fbc4a27587ac6295d7
Austin Kwon <austinkwon@Austins-MacBook-Pro.local>
2020-11-06 14:14:31 -0800

    Added search queries -AK

--- queries.sql
+++ queries.sql
@@ -1,18 +1,43 @@
 
--- Search query
+-- Search query ------------------------------------
+
 
 -- Get full product info
-SELECT s_Name, p_name, p_price, p_release, p_lastShipment
-FROM products
+SELECT *
+FROM products;
 
-SELECT s_storeName
-FROM Store
+SELECT *
+FROM Store;
 
-SELECT
+SELECT *
+FROM storeInventory;
+
+
+
+-- Check if a specific store contains a specific product
+SELECT p_name, s_storeNam
 FROM Contains
+WHERE s_storeNum = ' ' AND
+        p_name = ' ';
 
--- Insert query
+        
 
+
+
+
+
+-- Insert query ------------------------------------
+INSERT INTO Product(s_Name, p_name, p_price, p_release date, p_lastShipment[DATE] ), Store(s_cityID, s_storeName,  s_storeNum, p_Name)
+VALUES();
+
+INSERT INTO locations(storeName, storeNum, c_ID, c_name)
+VALUES();
+
+INSERT INTO SupplyDemand(p_name, p_price, p_lastShipment[DATE], r_nintendoRating, r_storeRating)
+VALUES();
+
+INSERT INTO priceChngFreq(p_name, p_price, p_lastShipment[DATE], b_name, b_basePrice)
+VALUES();
 
 
 -- Delete query ------------------------------------
@@ -24,6 +49,5 @@ WHERE
     p_Name = '' AND
     s_storeName = '' AND
     s_cityID = 1 AND
-    s_storeNum = 1 
-
+    s_storeNum = 1 ;
 
