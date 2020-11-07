cbccb327109bd069ee13a5acdfaa4a2b0fb9468d
Austin Kwon <austinkwon@Austins-MacBook-Pro.local>
2020-11-07 11:21:42 -0800

    Fixed insert query syntax

--- relTables.sql
+++ relTables.sql
@@ -16,14 +16,14 @@ CREATE TABLE saleFreq(
 CREATE TABLE Store(
     s_cityID INTEGER NOT NULL,
     s_storeName CHAR NOT NULL, 
-    s_storeNum INTEGER NOT NULL,
+    s_storeNum INTEGER NOT NULL
     );
 
 CREATE TABLE products(
     p_storeName CHAR NOT NULL,
     p_prodName CHAR NOT NULL,
     p_price DECIMAL NOT NULL,
-    p_releasedate DATE NOT NULL,
+    p_releasedate DATE NOT NULL
     );
 
 
@@ -51,7 +51,6 @@ CREATE TABLE storeInventory(
     sI_prodName CHAR NOT NULL,
     sI_storeName CHAR NOT NULL,
     sI_StoreNum INTEGER NOT NULL,
-    sI_prodName CHAR NOT NULL,
     sI_prodAmount INTEGER NOT NULL
     );
 
@@ -78,127 +77,123 @@ CREATE TABLE priceChngFreq(
 
 INSERT INTO 
     Contains(
-    c_prodName CHAR NOT NULL,
-    c_cityID INTEGER NOT NULL,
-    c_storeName CHAR NOT NULL,
-    c_storeNum INTEGER NOT NULL,
-    c_status BOOLEAN NOT NULL
+    c_prodName,
+    c_cityID,
+    c_storeName,
+    c_storeNum,
+    c_status
     )
 VALUES
-    ('The Legend of Zelda: Beadth of the Wild', 1, 'Target', 1, FALSE),
-    ('Super Mario Odessey', 1, 'Walmart', 2, TRUE)
+    ('The Legend of Zelda: Breath of the Wild', 1, 'Target', 1, FALSE),
+    ('Super Mario Odyssey', 1, 'Walmart', 2, TRUE),
     ('Animal Crossing: New Horizons', 2, 'Target', 2, TRUE);
 
 INSERT INTO
     saleFreq(
-    sf_prodName CHAR NOT NULL,
-    sf_storeName CHAR NOT NULL,
-    sf_lastShipment DATE NOT NULL
+    sf_prodName,
+    sf_storeName,
+    sf_lastShipment
     )
 VALUES
-    ('The Legend of Zelda: Beadth of the Wild', 'Target', '2020-09-02')
-    ('Super Mario Odessey', 'Walmart', '2020-08-10'),
+    ('The Legend of Zelda: Breath of the Wild', 'Target', '2020-09-02'),
+    ('Super Mario Odyssey', 'Walmart', '2020-08-10'),
     ('Animal Crossing: New Horizons', 'Target', '2020-09-28');
 
 INSERT INTO
     Store(
-    s_cityID INTEGER NOT NULL,
-    s_storeName CHAR NOT NULL, 
-    s_storeNum INTEGER NOT NULL,
+    s_cityID,
+    s_storeName, 
+    s_storeNum
     )
 VALUES
-    (1, 'Target', 1)
-    (1, 'Walmart', 1)
-    (1, 'Walmart', 2)
-    (1, 'Gamestop', 1)
-    (2, 'Target', 1)
-    (2, 'Target', 2)
-    (3, 'BestBuy', 1)
-    (3, 'Target', 1)
-    (3, 'Walmart', 1)
+    (1, 'Target', 1),
+    (1, 'Walmart', 1),
+    (1, 'Walmart', 2),
+    (1, 'Gamestop', 1),
+    (2, 'Target', 1),
+    (2, 'Target', 2),
+    (3, 'BestBuy', 1),
+    (3, 'Target', 1),
+    (3, 'Walmart', 1),
     (3, 'Gamestop', 1);
 
 INSERT INTO 
     products(
-    p_storeName CHAR NOT NULL,
-    p_prodName CHAR NOT NULL,
-    p_price DECIMAL NOT NULL,
-    p_releasedate DATE NOT NULL,
+    p_storeName,
+    p_prodName,
+    p_price,
+    p_releasedate
     )
 VALUES
     ('Target','The Legend of Zelda: Breadth of the Wild', 39.99, '2018-05-09'),
-    ('Walmart', 'Super Mario Odessey', 49.9, '2018-10-20'),
+    ('Walmart', 'Super Mario Odyssey', 49.9, '2018-10-20'),
     ('Target', 'Animal Crossing: New Horizons', 59.9, '2020-05-21');
 
 INSERT INTO
     inStock(
-    is_prodName CHAR NOT NULL,
-    is_storeName CHAR NOT NULL,
-    is_storeNum INTEGER NOT NULL,
-    is_cityID INTEGER NOT NULL,
-    is_prodAmount INTEGER NOT NULL
+    is_prodName,
+    is_storeName,
+    is_storeNum,
+    is_cityID,
+    is_prodAmount
     )
 VALUES
-    ('The Legend of Zelda: Breadth of the Wild', 'Target', 1, 1, 0)
-    ('Super Mario Odessey', 'Walmart', 2, 1, 5)
+    ('The Legend of Zelda: Breadth of the Wild', 'Target', 1, 1, 0),
+    ('Super Mario Odyssey', 'Walmart', 2, 1, 5),
     ('Animal Crossing: New Horizons', 'Target', 2, 2, 10);
 
 INSERT INTO
     shipmentETA(
-    sE_prodName CHAR NOT NULL,
-    sE_cityID INTEGER NOT NULL,
-    sE_storeName CHAR NOT NULL,
-    sE_storeNum INTEGER NOT NULL,
-    sE_lastShipment DATE NOT NULL,
-    sE_estimatedArrival DATE NOT NULL
+    sE_prodName,
+    sE_cityID,
+    sE_storeName,
+    sE_storeNum,
+    sE_lastShipment,
+    sE_estimatedArrival
     )
 VALUES
-    ('The Legend of Zelda: Breadth of the Wild', 1, 'Target', 1, '2020-09-02', '2020-09-16')
-    ('Super Mario Odessey', 1, 'Walmart', 2, '2020-08-10', '2020-08-24')
+    ('The Legend of Zelda: Breadth of the Wild', 1, 'Target', 1, '2020-09-02', '2020-09-16'),
+    ('Super Mario Odyssey', 1, 'Walmart', 2, '2020-08-10', '2020-08-24'),
     ('Animal Crossing: New Horizons', 2, 'Target', 2, '2020-09-28', '2020-10-12');
 
 INSERT INTO
     locations(
-    l_cityID INTEGER NOT NULL,
-    l_cityName CHAR NOT NULL
+    l_cityID,
+    l_cityName
     )
 VALUES
-    (1, 'Merced')
-    (2, 'Fresno')
-    (3, 'Modesto')
-    (4, 'Turlock')
+    (1, 'Merced'),
+    (2, 'Fresno'),
+    (3, 'Modesto'),
+    (4, 'Turlock'),
     (5, 'Madera');
 
 INSERT INTO
     supplyDemand(
-    sD_prodName CHAR NOT NULL,
-    sD_nintendoRating DECIMAL NOT NULL,
-    sD_storeRatingAvg DECIMAL NOT NULL
+    sD_prodName,
+    sD_nintendoRating,
+    sD_storeRatingAvg
     )
 VALUES  
-    ('The Legend of Zelda: Breadth of the Wild', 10, 9.4)
-    ('Super Mario Odessey', 9.1, 9.0)
+    ('The Legend of Zelda: Breadth of the Wild', 10, 9.4),
+    ('Super Mario Odyssey', 9.1, 9.0),
     ('Animal Crossing: New Horizons', 9.3, 8.8);
 
 INSERT INTO
     priceChngFreq(
-    pCF_prodName CHAR NOT NULL,
-    pCF_storeName CHAR NOT NULL,
-    pCF_prodStorePrice DECIMAL NOT NULL,
-    pCF_basePrice DECIMAL NOT NULL,
-    pCF_percentChng DECIMAL NOT NULL
+    pCF_prodName,
+    pCF_storeName,
+    pCF_prodStorePrice,
+    pCF_basePrice,
+    pCF_percentChng
     )
 VALUES
-    ('The Legend of Zelda: Breadth of the Wild', 'Target', 39.99, 49.99, -20.01)
-    ('Super Mario Odessey', 'Walmart', 49.99, 30.00, 40.01)
+    ('The Legend of Zelda: Breadth of the Wild', 'Target', 39.99, 49.99, -20.01),
+    ('Super Mario Odyssey', 'Walmart', 49.99, 30.00, 40.01),
     ('Animal Crossing: New Horizons', 'Target', 49.99, 60.00, -17.02);
 
 
-
-
-
-
-
+------------------------------------------------------------------------------
 DROP TABLE IF EXISTS Contains;
 DROP TABLE IF EXISTS saleFreq;
 DROP TABLE IF EXISTS Store;
