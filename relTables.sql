6d9ec030b8fca0b941fe9145de88d67fb197f28f
Rodolfo Higuera <43897359+OFO323@users.noreply.github.com>
2020-11-06 16:01:15 -0800

    updated/populated tables
    had to make a couple of changes to the current tables and cleaned it up a bit. Each table should have about ~3 tuples. We may need to visit our current queries to account for these changes. Feel free to look over the changes and let me know if you think something needs to be tweaked!

--- relTables.sql
+++ relTables.sql
@@ -1,72 +1,202 @@
 --tables
 CREATE TABLE Contains(
-    p_Name CHAR NOT NULL,
-    c_ID INTEGER NOT NULL,
-    s_storeNam CHAR NOT NULL,
-    s_storeNum INTEGER NOT NULL
+    c_prodName CHAR NOT NULL,
+    c_cityID INTEGER NOT NULL,
+    c_storeName CHAR NOT NULL,
+    c_storeNum INTEGER NOT NULL,
+    c_status BOOLEAN NOT NULL -- shows if store contains product
     );
 
 CREATE TABLE saleFreq(
-    p_Name CHAR NOT NULL,
-    s_storeName CHAR NOT NULL,
-    p_lastShipment DATE NOT NULL
+    sf_prodName CHAR NOT NULL,
+    sf_storeName CHAR NOT NULL,
+    sf_lastShipment DATE NOT NULL
     );
 
 CREATE TABLE Store(
     s_cityID INTEGER NOT NULL,
     s_storeName CHAR NOT NULL, 
     s_storeNum INTEGER NOT NULL,
-    p_Name CHAR NOT NULL
     );
 
 CREATE TABLE products(
-    s_Name CHAR NOT NULL,
-    p_name CHAR NOT NULL,
+    p_storeName CHAR NOT NULL,
+    p_prodName CHAR NOT NULL,
     p_price DECIMAL NOT NULL,
-    p_release DATE NOT NULL,
-    p_lastShipment DATE NOT NULL
+    p_releasedate DATE NOT NULL,
     );
+
 
 CREATE TABLE inStock(
-    p_Name CHAR NOT NULL,
-    i_storeNum INTEGER NOT NULL,
-    i_prodAmount INTEGER NOT NULL
+    is_prodName CHAR NOT NULL,
+    is_storeName CHAR NOT NULL,
+    is_storeNum INTEGER NOT NULL,
+    is_cityID INTEGER NOT NULL,
+    is_prodAmount INTEGER NOT NULL
     );
+
+
 
 CREATE TABLE shipmentETA(
-    p_Name CHAR NOT NULL,
-    i_storeNum INTEGER NOT NULL,
-    i_lastShipment DATE NOT NULL
+    sE_prodName CHAR NOT NULL,
+    sE_cityID INTEGER NOT NULL,
+    sE_storeName CHAR NOT NULL,
+    sE_storeNum INTEGER NOT NULL,
+    sE_lastShipment DATE NOT NULL,
+    sE_estimatedArrival DATE NOT NULL
     );
 
+--redundant, same as inStock
 CREATE TABLE storeInventory(
-    p_Name CHAR NOT NULL,
-    i_storeName CHAR NOT NULL,
-    i_StoreNum INTEGER NOT NULL,
-    i_prodName CHAR NOT NULL,
-    i_prodAmount INTEGER NOT NULL
+    sI_prodName CHAR NOT NULL,
+    sI_storeName CHAR NOT NULL,
+    sI_StoreNum INTEGER NOT NULL,
+    sI_prodName CHAR NOT NULL,
+    sI_prodAmount INTEGER NOT NULL
     );
 
 CREATE TABLE locations(
-    storeName CHAR NOT NULL,
-    storeNum INTEGER NOT NULL,
-    c_ID INTEGER NOT NULL,
-    c_name CHAR NOT NULL
+    l_cityID INTEGER NOT NULL,
+    l_cityName CHAR NOT NULL
     );
 
-CREATE TABLE supplyDemand(p_name CHAR NOT NULL,
-    p_price DECIMAL NOT NULL,
-    p_lastShipment DATE NOT NULL,
-    r_nintendoRating DECIMAL NOT NULL,
-    r_storeRating DECIMAL NOT NULL
+CREATE TABLE supplyDemand(
+    sD_prodName CHAR NOT NULL,
+    sD_nintendoRating DECIMAL NOT NULL,
+    sD_storeRatingAvg DECIMAL NOT NULL
     );
 
 CREATE TABLE priceChngFreq(
-    p_name CHAR NOT NULL,
-    p_price DECIMAL NOT NULL,
-    p_lastShipment DATE NOT NULL,
-    b_name CHAR NOT NULL, b_basePrice DECIMAL NOT NULL
+    pCF_prodName CHAR NOT NULL,
+    pCF_storeName CHAR NOT NULL,
+    pCF_prodStorePrice DECIMAL NOT NULL,
+    pCF_basePrice DECIMAL NOT NULL,
+    pCF_percentChng DECIMAL NOT NULL
     );
+
+------------------------------------------------------------------------------
+
+INSERT INTO 
+    Contains(
+    c_prodName CHAR NOT NULL,
+    c_cityID INTEGER NOT NULL,
+    c_storeName CHAR NOT NULL,
+    c_storeNum INTEGER NOT NULL,
+    c_status BOOLEAN NOT NULL
+    )
+VALUES
+    ('The Legend of Zelda: Beadth of the Wild', 1, 'Target', 1, FALSE),
+    ('Super Mario Odessey', 1, 'Walmart', 2, TRUE)
+    ('Animal Crossing: New Horizons', 2, 'Target', 2, TRUE)
+
+INSERT INTO
+    saleFreq(
+    sf_prodName CHAR NOT NULL,
+    sf_storeName CHAR NOT NULL,
+    sf_lastShipment DATE NOT NULL
+    )
+VALUES
+    ('The Legend of Zelda: Beadth of the Wild', 'Target', '2020-09-02')
+    ('Super Mario Odessey', 'Walmart', '2020-08-10'),
+    ('Animal Crossing: New Horizons', 'Target', '2020-09-28')
+
+INSERT INTO
+    Store(
+    s_cityID INTEGER NOT NULL,
+    s_storeName CHAR NOT NULL, 
+    s_storeNum INTEGER NOT NULL,
+    )
+VALUES
+    (1, 'Target', 1)
+    (1, 'Walmart', 1)
+    (1, 'Walmart', 2)
+    (1, 'Gamestop', 1)
+    (2, 'Target', 1)
+    (2, 'Target', 2)
+    (3, 'BestBuy', 1)
+    (3, 'Target', 1)
+    (3, 'Walmart', 1)
+    (3, 'Gamestop', 1)
+
+INSERT INTO 
+    products(
+    p_storeName CHAR NOT NULL,
+    p_prodName CHAR NOT NULL,
+    p_price DECIMAL NOT NULL,
+    p_releasedate DATE NOT NULL,
+    )
+VALUES
+    ('Target','The Legend of Zelda: Breadth of the Wild', 39.99, '2018-05-09'),
+    ('Walmart', 'Super Mario Odessey', 49.9, '2018-10-20'),
+    ('Target', 'Animal Crossing: New Horizons', 59.9, '2020-05-21')
+
+INSERT INTO
+    inStock(
+    is_prodName CHAR NOT NULL,
+    is_storeName CHAR NOT NULL,
+    is_storeNum INTEGER NOT NULL,
+    is_cityID INTEGER NOT NULL,
+    is_prodAmount INTEGER NOT NULL
+    )
+VALUES
+    ('The Legend of Zelda: Breadth of the Wild', 'Target', 1, 1, 0)
+    ('Super Mario Odessey', 'Walmart', 2, 1, 5)
+    ('Animal Crossing: New Horizons', 'Target', 2, 2, 10)
+
+INSERT INTO
+    shipmentETA(
+    sE_prodName CHAR NOT NULL,
+    sE_cityID INTEGER NOT NULL,
+    sE_storeName CHAR NOT NULL,
+    sE_storeNum INTEGER NOT NULL,
+    sE_lastShipment DATE NOT NULL,
+    sE_estimatedArrival DATE NOT NULL
+    )
+VALUES
+    ('The Legend of Zelda: Breadth of the Wild', 1, 'Target', 1, '2020-09-02', '2020-09-16')
+    ('Super Mario Odessey', 1, 'Walmart', 2, '2020-08-10', '2020-08-24')
+    ('Animal Crossing: New Horizons', 2, 'Target', 2, '2020-09-28', '2020-10-12')
+
+INSERT INTO
+    locations(
+    l_cityID INTEGER NOT NULL,
+    l_cityName CHAR NOT NULL
+    )
+VALUES
+    (1, 'Merced')
+    (2, 'Fresno')
+    (3, 'Modesto')
+    (4, 'Turlock')
+    (5, 'Madera')
+
+INSERT INTO
+    supplyDemand(
+    sD_prodName CHAR NOT NULL,
+    sD_nintendoRating DECIMAL NOT NULL,
+    sD_storeRatingAvg DECIMAL NOT NULL
+    )
+VALUES  
+    ('The Legend of Zelda: Breadth of the Wild', 10, 9.4)
+    ('Super Mario Odessey', 9.1, 9.0)
+    ('Animal Crossing: New Horizons', 9.3, 8.8)
+
+INSERT INTO
+    priceChngFreq(
+    pCF_prodName CHAR NOT NULL,
+    pCF_storeName CHAR NOT NULL,
+    pCF_prodStorePrice DECIMAL NOT NULL,
+    pCF_basePrice DECIMAL NOT NULL,
+    pCF_percentChng DECIMAL NOT NULL
+    )
+VALUES
+    ('The Legend of Zelda: Breadth of the Wild', 'Target', 39.99, 49.99, -20.01)
+    ('Super Mario Odessey', 'Walmart', 49.99, 30.00, 40.01)
+    ('Animal Crossing: New Horizons', 'Target', 49.99, 60.00, -17.02)
+
+
+
+
+
 
 
 DROP TABLE IF EXISTS Contains;
