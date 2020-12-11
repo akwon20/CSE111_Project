cc3aab69e8f3ee7be0c22d25931fc269ecde8999
Rodolfo Higuera <43897359+OFO323@users.noreply.github.com>
2020-12-10 16:08:45 -0800

    Update h_Page.js

--- client/src/pages/h_Page.js
+++ client/src/pages/h_Page.js
@@ -1,57 +1,91 @@
 import React, { Component, Fragment} from 'react';
+import {Link} from "react-router-dom";
+//import axios from 'axios';
 //import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Table from 'react-bootstrap/Table';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 
+//import moment from 'moment';
+
+// Set form of pulled data (Complete when queries are ready)
+// const itemlist = item => (
+    // <tr>
+    //     <td>item.list.</td>
+    //     <td>item.list.</td>
+    //     <td>item.list.</td>
+    //     <td>item.list.</td>
+    // </tr>
+// )
 
 
 
 class Hardware extends Component {
 
+    constructor(props){
+        super(props)
+        this.state = {
+            isLoaded: false,
+            list: []
+        }
+    }
+
+    //when componenet is successfully called pull appropriate data
+    async componentDidMount(){
+        // this.getHardware();
+
+
+        //this is more 
+        const url = "http://localhost:5000/routes/hardware";
+        const response = await fetch (url);
+        const data = await response.json();
+        this.setState({list : data});
+        }
+
+
+
+
 
     render() {
 
+        const {list} = this.state;
+
         return(
             <Fragment>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Nintendo Tingz</Navbar.Brand>
-                </Navbar>
+                <Link to="/">
+                    <Navbar bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">Nintendo Tingz</Navbar.Brand>
+                    </Navbar>
+                </Link>
                 <Jumbotron>
                     <h1>Hardware</h1>
                     <p>
-                        This page holds software products like the latest games in the Nintendo library
+                        This page holds hardware products like consoles and other accessories
                     </p>
                 </Jumbotron>
                 <Container>
-                    <Table style = {{color:"#f72525"}} striped hover >
-                        <p>contains place holders for now</p>
-                        <br></br>
+                    <Table style = {{color:"#f72525"}} striped hover >                        <br></br>
                         
                         <thead>
                             <td>Product</td>
                             <td>Price</td>
-                            <td>Rating</td>
                             <td>Release Date</td>
                         </thead>
                         <tbody>
                             {/* this is where data will be pulled and displayed */}
-                            <tr>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                            </tr>
-                            <tr>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                            </tr>
+                            {list.map((item, index) => {
+                                return  <tr>
+                                            <Link to = {{pathname: `/product`, state : {
+                                            }}}>
+                                            <td>{item.h_name}</td></Link>
+                                            <td>{item.h_price}</td>
+                                            <td>{item.h_releaseDate}</td>
+                                        </tr>
+                            })}
                         </tbody>
                     </Table>
                 </Container>
