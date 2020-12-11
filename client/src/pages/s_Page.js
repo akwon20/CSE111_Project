a1dfe5c948631fc2b32aba7c1c8b85cac28e969d
Rodolfo Higuera <43897359+OFO323@users.noreply.github.com>
2020-12-10 16:09:11 -0800

    Update s_Page.js

--- client/src/pages/s_Page.js
+++ client/src/pages/s_Page.js
@@ -1,25 +1,25 @@
 import React, { Component, Fragment} from 'react';
-//import Container from 'react-bootstrap/Container';
+import {Link} from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import Nav from 'react-bootstrap/Nav';
 import Table from 'react-bootstrap/Table';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 
-import moment from 'moment';
+//import moment from 'moment';
+
+//var curr_time = moment().utcOffset('0').format('YYYY-MM-DD'); //may use this for situations where we sort by most recent 
+
 
 //set form of pulled data [complete when queries are finished]
-const itemlist = item => (
-    <tr>
-        <td>item.list.</td>
-        <td>item.list.</td>
-        <td>item.list.</td>
-        <td>item.list.</td>
-
-    </tr>
-)
+// const itemDetails = ({item}) => (
+//     <tr>
+//         <td>{item.s_prodName}</td>
+//         <td>{item.s_price}</td>
+//         <td>{item.s_releasedate}</td>
+//     </tr>
+// )
 
 
 class Software extends Component {
@@ -32,35 +32,50 @@ class Software extends Component {
     }
 
     //when componenet is successfully called pull appropriate data
-    componentDidMount(){
-        this.getSoftware();
+    async componentDidMount(){
+        //this.getSoftware();
+
+        const url = "http://localhost:5000/routes/software";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({list : data});
+        //console.log(this.state.list);
     }
 
 
     //set pulled data to the form of the list
-    S_List = () => {
-        return this.state.list.map((curr, index) => {
-                return < itemlist list = {curr} key = {index} />
-        })
+    // S_List = () => {
+    //     return this.state.list.map((item, index) => {
+    //             return <itemDetails list = {item} key = {index} />
+    //     })
+    // }
 
-    }
 
     //pull data from the DB
-    getSoftware(){
-        var curr_time = moment().utcOffset('0').format('YYYY-MM-DD'); //may use this for situations where we sort by most recent 
+    // getSoftware(){
 
-        fetch(`http://localhost:5000/`) //need to complete queries 
-            .then(res => res.json()) //put result in json for to extract
-            .then(list => this.setState({list})) //set pulled data to current list
-            .catch(err => console.log(err))
-    }
+    //     fetch(`http://localhost:5000/routes/software`) //need to complete queries 
+    //         .then(res => res.json())
+    //         .then(res => { console.log(res); return res; }) //
+    //         .then(list => this.setState({list}))
+    //         .catch(err => console.log(err))
+
+    //     //console.log(this.state);
+    // }
+
+    
 
     render() {
+        //console.log(this.state);
+        const {list} = this.state;
+
         return(
             <Fragment>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Nintendo Tingz</Navbar.Brand>
-                </Navbar>
+                <Link to="/">
+                    <Navbar bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">Ninventory</Navbar.Brand>
+                    </Navbar>
+                </Link>
                 <Jumbotron>
                     <h1>Software</h1>
                     <p>
@@ -69,29 +84,21 @@ class Software extends Component {
                 </Jumbotron>
                 <Container bg = "#f72525">
                     <Table style = {{color:"#f72525"}} striped hover >
-                        <p>contains place holders for now</p>
-                        <br></br>
-
                         <thead>
                             <td>Product</td>
                             <td>Price</td>
-                            <td>Rating</td>
                             <td>Release Date</td>
                         </thead>
                         <tbody>
-                            {/* this is where pulled data is displayed */}
-                            <tr>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                            </tr>
-                            <tr>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                                <td>Sample</td>
-                            </tr>
+                            {list.map((item, index) => {
+                                return <tr>
+                                        <Link to = {{pathname: `/product`, state : {
+                                            }}}>
+                                            <td>{item.s_prodName}</td></Link>
+                                            <td>{item.s_price}</td>
+                                            <td>{item.s_releasedate}</td>
+                                        </tr>
+                            })}
                         </tbody>
                     </Table>
                 </Container>
